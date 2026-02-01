fd2c6911437598e2b84a4d58114ab1033325c016
Aneesh <aneesh.muthyala8@gmail.com>
2026-02-01 18:52:09 +0530

    Updated to another approach

---
+++ src/main/java/com/example/demo/DemoLambdaHandler.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class DemoLambdaHandler implements RequestHandler<String, String> {
+
+    @Override
+    public String handleRequest(String input, Context context) {
+        return "Processed XML: " + input;
+    }
+}
+
+
