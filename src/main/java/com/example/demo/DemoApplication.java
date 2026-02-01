a7c413d72d2cd216f8a69e6a9b88e90ac7c68452
Aneesh <aneesh.muthyala8@gmail.com>
2026-02-01 17:25:41 +0530

    updated pom again

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
+
